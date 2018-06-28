705e5e0e0b70a4a0b5c1c4ec5e066395de571215
code-mridul <mridulchowdhury93@gmail.com>
2018-06-28 15:53:29 +0600

    create user schema and database

--- server.js
+++ server.js
@@ -1,21 +1,39 @@
 var express = require('express');
 var morgan = require('morgan');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
+var User = require('./models/user');
 
 var app = express();
 
-//Middleware
-app.use(morgan('dev'));
-
-
-
-app.get('/', function(req,res){
-    var name = "Mridul";
-    res.json("my name is " + name);
+mongoose.connect('mongodb://root:abcd1727@ds121331.mlab.com:21331/ecommerce', function(err) {
+    if (err) {
+        console.log(err);
+    } else {
+      console.log("Connected to Database");
+    }
 });
 
-app.get('/lastname', function(req,res){
-    res.json("Chowdhury");
+
+// Middleware
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
+
+
+app.post('/create-user', function(req, res, next) {
+    var user = new User();
+
+    user.profile.name = req.body.name;
+    user.email = req.body.email;
+    user.password = req.body.password;
+
+    user.save(function(err) {
+        if (err) return next(err);
+        res.json('successfully create a new user');
+    });
 });
 
 app.listen(3000, function(err) {
